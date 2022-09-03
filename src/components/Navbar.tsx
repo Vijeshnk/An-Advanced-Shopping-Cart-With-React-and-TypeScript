33e09e286435ec659a0dbb2782ceb264d693cd41
vijeshnk <vijeshnirala@gmail.com>
2022-09-03 08:53:39 +0530

    Shopping Cart Context

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Button, Container, Nav, Navbar as NavbarBs } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
+import { useShoppingCart } from '../context/shoppingCartContext'
 
 const Navbar = () => {
+
+  const {openCart,cartQuantity} = useShoppingCart()
+
+
   return (
     <NavbarBs sticky='top' className='bg-white shadow-sm mb-3'>
         <Container>
@@ -20,7 +25,8 @@ const Navbar = () => {
                 </Nav.Link>
 
                 </Nav>
-                <Button style={{width:"3rem",height:"3rem",position:'relative'}} variant="outline-primary" className='rounded-circle'>
+
+                {cartQuantity > 0 && (<Button onClick={openCart} style={{width:"3rem",height:"3rem",position:'relative'}} variant="outline-primary" className='rounded-circle'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
   <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
 </svg>
@@ -32,11 +38,18 @@ style={{
     position:"absolute",
     bottom:0,
     right:0,
-    transform:"translate(25%,25%)"}}>3</div>
+    transform:"translate(25%,25%)",
+    }}
+    >
+      {cartQuantity}
+      </div>
                 </Button>
+                )}
         </Container>
     </NavbarBs>
   )
 }
+  
+
 
 export default Navbar
--- src/components/StoreItem .tsx
+++ src/components/StoreItem .tsx
@@ -1,5 +1,5 @@
-import React from 'react'
 import { Button, Card } from 'react-bootstrap'
+import { useShoppingCart } from '../context/shoppingCartContext'
 import formatCurrency from '../utilities/formatCurrency'
 
 type StoreItemProps={
@@ -11,7 +11,8 @@ type StoreItemProps={
 
 const StoreItem  = ({id,name,price,imgUrl}:StoreItemProps) => {
 
-const quantity =0
+  const {getItemQuantity,increaseCartQuantity,decreaseCartQuantity,removeFromCart} = useShoppingCart()
+const quantity =getItemQuantity(id)
 
   return (
     <Card className='h-100'>
@@ -26,17 +27,17 @@ const quantity =0
           <span className='ms-2 text-muted'>{formatCurrency(price)}</span>
         </Card.Title>
         <div className='mt-auto'>
-              {quantity ===0 ? (<Button className='w-100'>+ Add To Cart</Button>):(<div className='d-flex align-items-center flex-column' style={{gap:"0.5rem"}}>
+              {quantity ===0 ? (<Button className='w-100' onClick={()=> increaseCartQuantity(id)}>+ Add To Cart</Button>):(<div className='d-flex align-items-center flex-column' style={{gap:"0.5rem"}}>
               <div className='d-flex align-items-center justify-content-center' style={{gap:"0.5rem"}}>
-                  <Button>+</Button>
+                  <Button onClick={()=> decreaseCartQuantity(id)}>-</Button>
                   <div>
                   <span className='fs-3'>{quantity}</span> 
                   in cart
                   </div>
                   
-                  <Button>-</Button>
+                  <Button onClick={()=> increaseCartQuantity(id)}>+</Button>
               </div>
-              <Button variant='danger' size='sm'>Remove</Button>
+              <Button variant='danger' size='sm' onClick={()=> removeFromCart(id)}>Remove</Button>
                 </div>
                 )}
         </div>
